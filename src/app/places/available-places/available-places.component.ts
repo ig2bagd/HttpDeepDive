e631f40998ac81b6a69d87843fc7045502b65c76
steviep <ig2bagd@yahoo.com>
2024-07-02 20:08:04 -0700

    first commit

---
+++ src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,62 @@
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { PlacesComponent } from '../places.component';
+import { PlacesService } from '../places.service';
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
+  private placesService = inject(PlacesService);
+  private destroyRef = inject(DestroyRef);
+
+  // constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.isFetching.set(true);
+    // const subscription = this.httpClient
+    //   .get<{ places: Place[] }>('http://localhost:3000/places', {
+    //     observe: 'response'       // Or events
+    //   })
+    //   .subscribe({
+    //     next: (response) => {
+    //       console.log(response);
+    //       console.log(response.body?.places);
+    //     },
+    //   });
+    const subscription = this.placesService.loadAvailablePlaces().subscribe({
+      next: (places) => {
+        this.places.set(places);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+      error: (error: Error) => {
+        this.error.set(error.message);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onSelectPlace(selectedPlace: Place) {
+    const subscription = this.placesService.addPlaceToUserPlaces(selectedPlace).subscribe({
+      next: (resData) => console.log(resData),
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+}
